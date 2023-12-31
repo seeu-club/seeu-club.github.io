54ef52058c062973fbe0472d5d7cdde032cf4347
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-12-31 18:27:13 +0800

    fix links

--- src/components/layout/footer.jsx
+++ src/components/layout/footer.jsx
@@ -8,12 +8,12 @@ export default function Footer() {
             {/* <a className="media" href="#" target="_blank">
                 <img src={MediaImg} alt=""/>
             </a> */}
-            <a className="media" href="#" target="https://twitter.com/seeu_brc20">
+            <a className="media" href="https://twitter.com/seeu_brc20" target="https://twitter.com/seeu_brc20">
                 <span role="img"  className="anticon anticon-twitter">
                     <img src={TwitterImg} alt=""/>
                 </span>
             </a>
-            <a className="media" href="#" target="https://t.me/seeubtc">
+            <a className="media" href="#https://t.me/seeubtc" target="https://t.me/seeubtc">
                 <img src={TelegramImg} alt=""/>
         </a></div>
     </div>
